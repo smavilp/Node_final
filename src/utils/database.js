71a42ee7eb120d44b5fd805fd5f16e220608ed23
Sebastián <114272263+smavilp@users.noreply.github.com>
2023-06-13 08:21:15 -0500

    adding dialectOptions

--- src/utils/database.js
+++ src/utils/database.js
@@ -7,7 +7,8 @@ const db = new Sequelize({
   database: process.env.DB_NAME,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  dialect: process.env.DB_DIALECT
+  dialect: process.env.DB_DIALECT,
+  dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }
 });
 
 module.exports = db;
